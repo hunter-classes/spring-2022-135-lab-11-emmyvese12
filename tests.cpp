ef284bb87483ade7a688162f912f66992a1a055a
Emmy Veselinov <emmyveselinov@gmail.com>
2022-04-13 18:59:43 -0400

    added test cases for task A

--- tests.cpp
+++ tests.cpp
@@ -1,5 +1,6 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "profile.h"
+#include "doctest.h"
 
 // g++ -std=c++11 -c tests.cpp
 
@@ -7,6 +8,11 @@
 TEST_CASE("Get Username for TASK A"){
     Profile prof1A("emmyvese12", "Emmy");
     Profile prof2A("lunabellzz", "Lunaaa");
+    Profile prof3A;
     CHECK(prof1A.getUsername() == "emmyvese12");
     CHECK(prof2A.getUsername() == "lunabellzz");
+    CHECK(prof3A.getUsername() == "");
+    CHECK(prof1A.getFullName() == "Emmy (@emmyvese12)");
+    CHECK(prof2A.getFullName() == "Lunaaa (@lunabellzz)");
+    CHECK(prof3A.getFullName() == " (@)");
 }
