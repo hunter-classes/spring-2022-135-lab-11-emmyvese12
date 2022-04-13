e68e55c772b621e729878bc3b5a28cf1d290c940
Emmy Veselinov <emmyveselinov@gmail.com>
2022-04-13 00:46:05 -0400

    test cases still in progress

--- tests.cpp
+++ tests.cpp
@@ -1,11 +1,10 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "profile.h"
-#include <string>
 
 // g++ -std=c++11 -c tests.cpp
 
 //task A
-TEST_CASE("Get username for TASK A"){
+TEST_CASE("Get Username for TASK A"){
     Profile prof1A("emmyvese12", "Emmy");
     Profile prof2A("lunabellzz", "Lunaaa");
     CHECK(prof1A.getUsername() == "emmyvese12");
