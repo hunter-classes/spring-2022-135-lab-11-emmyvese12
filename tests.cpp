14f1cdf816a1eba0cc15116d20dffc99f4878a8a
Emmy Veselinov <emmyveselinov@gmail.com>
2022-04-13 22:36:58 -0400

    added test cases for task A and B

--- tests.cpp
+++ tests.cpp
@@ -1,18 +1,33 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "profile.h"
+#include "network.h"
 #include "doctest.h"
 
 // g++ -std=c++11 -c tests.cpp
 
 //task A
-TEST_CASE("Get Username for TASK A"){
-    Profile prof1A("emmyvese12", "Emmy");
-    Profile prof2A("lunabellzz", "Lunaaa");
-    Profile prof3A;
-    CHECK(prof1A.getUsername() == "emmyvese12");
-    CHECK(prof2A.getUsername() == "lunabellzz");
-    CHECK(prof3A.getUsername() == "");
-    CHECK(prof1A.getFullName() == "Emmy (@emmyvese12)");
-    CHECK(prof2A.getFullName() == "Lunaaa (@lunabellzz)");
-    CHECK(prof3A.getFullName() == " (@)");
+TEST_CASE("Get Username and Display Name for TASK A"){
+    Profile prof1("emmyvese12", "Emmy");
+    Profile prof2("lunabellzz", "Lunaaa");
+    Profile prof3;
+    Profile prof4("thelori", "Lori");
+    CHECK(prof1.getUsername() == "emmyvese12");
+    CHECK(prof2.getUsername() == "lunabellzz");
+    CHECK(prof3.getUsername() == "");
+    CHECK(prof1.getFullName() == "Emmy (@emmyvese12)");
+    CHECK(prof2.getFullName() == "Lunaaa (@lunabellzz)");
+    CHECK(prof3.getFullName() == " (@)");
+    CHECK(prof4.getUsername() == "thelori");
+    CHECK(prof4.getFullName() == "Lori (@thelori)");
+}
+
+//task B
+TEST_CASE("Adding User to the Network for TASK B"){
+    Network nw1, nw2, nw3, nw4, nw5;
+    CHECK(nw1.addUser("emmy", "Emmy") == 1);
+    CHECK(nw2.addUser("kris7", "Kris") == 1);
+    CHECK(nw3.addUser("luna!", "Luna") == 0);
+    CHECK(nw4.addUser("ash-2", "Ash") == 0);
+    CHECK(nw5.addUser("liz zy", "Liz") == 0);
+    CHECK(nw5.addUser("drewdrew", "D") == 1);
 }
