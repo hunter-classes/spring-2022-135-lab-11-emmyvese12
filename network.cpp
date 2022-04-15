8d0ebb73c3d50a455fe34f38f2aa695601d5b3e1
Emmy Veselinov <emmyveselinov@gmail.com>
2022-04-15 00:08:08 -0400

    added more class functions for task C

--- network.cpp
+++ network.cpp
@@ -7,6 +7,8 @@
 //TASK B
 
 //private class function
+
+// Returns user ID (index in the 'profiles' array) by their username
 int Network::findID(std::string usrn){
 
     for (int i = 0; i < MAX_USERS; i++){
@@ -20,7 +22,22 @@ int Network::findID(std::string usrn){
     return -1; //user not found in the profiles array
 }
 
-Network::Network() : numUsers(0) { }
+//Network::Network() : numUsers(0) { }
+Network::Network(){
+    numUsers = 0;
+    //set all elements of the matrix following to be false --> empty network so no one is following anyone
+    for (int i = 0; i < MAX_USERS; i++){
+        for (int j = 0; j < MAX_USERS; j++){
+            following[i][j] = false;
+        }
+    }
+    
+}
+
+
+
+// Attempts to sign up a new user with specified username and displayname
+// return true if the operation was successful, otherwise return false
 
 bool Network::addUser(std::string usrn, std::string dspn){
     //user can only be signed up if:
@@ -49,4 +66,46 @@ bool Network::addUser(std::string usrn, std::string dspn){
         return true;
 
     }
+}
+
+// Make 'usrn1' follow 'usrn2' (if both usernames are in the network).
+// return true if success (if both usernames exist), otherwise return false
+bool Network::follow(std::string usrn1, std::string usrn2){
+    if (findID(usrn1) != -1 && findID(usrn2) != -1){
+        //that means both users are found in the network, continue w/ next steps
+        int user1ID = findID(usrn1);
+        int user2ID = findID(usrn2);
+
+        following[user1ID][user2ID] = true;
+        //user1 is following user2
+
+        return true;
+    }
+
+    return false;
+    
+}
+
+// Print Dot file (graphical representation of the network)
+void Network::printDot(){
+    std::cout << "digraph {" << std::endl;
+    for (int i = 0; i < numUsers; i++){
+        std::cout << '"' << "@" << profiles[i].getUsername() << '"' << std::endl;
+    }
+    std::cout << "\n";
+
+   
+    for (int i = 0; i < numUsers; i++){
+        for (int j = 0; j < numUsers; j++){
+            if (following[i][j] == true){
+                std::string usr1 = profiles[i].getUsername();
+                std::string usr2 = profiles[j].getUsername();
+                std::cout << '"' << "@" << usr1 << '"' << " -> " << '"' << "@" << usr2 << '"' << std::endl;
+
+            }
+        }
+    }
+    std::cout << "}" << std::endl;
+
+
 }
