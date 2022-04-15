cf26851c005b4bc2695eccd72f9e16329faf7c8e
Emmy Veselinov <emmyveselinov@gmail.com>
2022-04-15 00:06:48 -0400

    updated Network class

--- network.h
+++ network.h
@@ -9,15 +9,19 @@ class Network {
         Profile profiles[MAX_USERS];     // user profiles array:
                                         // mapping integer ID -> Profile
 
-        // Returns user ID (index in the 'profiles' array) by their username
-        // (or -1 if username is not found)
+        
         int findID (std::string usrn);
+
+        bool following[MAX_USERS][MAX_USERS];  // friendship matrix:
 
     public:
         // Constructor, makes an empty network (numUsers = 0)
         Network();
 
-        // Attempts to sign up a new user with specified username and displayname
-        // return true if the operation was successful, otherwise return false
+    
         bool addUser(std::string usrn, std::string dspn);
+        
+        bool follow(std::string usrn1, std::string usrn2);
+
+        void printDot();
 };
